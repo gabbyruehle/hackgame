9a174caf616cb1191ae217df60cd1bc3885760d7
ellierk <38147329+ellierk@users.noreply.github.com>
2018-04-07 02:22:33 -0400

    bullet 1

--- game.ts
+++ game.ts
@@ -10,6 +10,18 @@ document.body.appendChild(app.view);
 let background: Sprite = Sprite.fromImage("./desert.jpg");
 app.stage.addChild(background);
 
+class Bullet1 {
+    sprite: Sprite; 
+    direction: number = 1;
+    constructor() {
+        this.sprite = Sprite.fromImage("bullet.png");
+        this.sprite.y = shooter1.y;
+        this.sprite.x = 40;
+        this.sprite.scale.x = .12;
+        this.sprite.scale.y = .12;
+    }
+}
+
 let shooter1: Sprite = Sprite.fromImage("katniss-edit.jpg");
 shooter1.scale.x = .3;
 shooter1.scale.y = .3;
@@ -35,6 +47,10 @@ window.addEventListener("keydown", (e: KeyboardEvent): void => {
     const STEP: number = 5;
     if (e.keyCode === SHOOT1) {
         // ADD NEW BULLET SHOOTER1
+        // S1 = true;
+        let B: Bullet1 = new Bullet1;
+        app.stage.addChild(B.sprite);
+    } 
     } else if (e.keyCode === UP1) {
         shooter1.y -= STEP;
     } else if (e.keyCode === DOWN1) {
