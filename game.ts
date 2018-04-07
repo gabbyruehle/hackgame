d1a0a2e83898d5ee8b411ee9b6d4555de437d764
gabbyruehle <38147333+gabbyruehle@users.noreply.github.com>
2018-04-06 22:56:10 -0400

    Update game.ts

--- game.ts
+++ game.ts
@@ -3,3 +3,11 @@ import {
   Container,
   Application
 } from "pixi.js";
+
+
+let shooter2: Sprite = Sprite.fromImage("./tonsofthings.jpg");
+shooter2.scale.x = .5;
+shooter2.scale.y = .5;
+shooter2.x = 500;
+shooter2.y = 240;
+app.stage.addChild(shooter2);
